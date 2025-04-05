35146c10030a62d77ef7ee0f9692acbabf422abb
Alisher <alisher@MacBook-Air-Alisher.local>
2025-04-05 20:55:25 +0300

    chore: обновлены зависимости и конфигурация
    - Обновлен Prisma до версии 6.5.0
    - Исправлен синтаксис CORS origins
    - Обновлены зависимости проекта

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,7 +17,7 @@ export const config = {
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'http://localhost:3000',
-      'http://127.0.0.1:3000',
+      'http://127.0.0.1:3000'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-telegram-id', 'x-telegram-init-data'],
